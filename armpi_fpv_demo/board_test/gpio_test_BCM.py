253402499e8ad924d7076f8700e1a0c4dd8e7abb
HsunCheng1214 <209410314@gms.tku.edu.tw>
2023-10-26 07:38:03 +0000

    10/26 commit

--- armpi_fpv_demo/board_test/gpio_test_BCM.py
+++ armpi_fpv_demo/board_test/gpio_test_BCM.py
@@ -5,27 +5,50 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Set the GPIO pin number you connected the button to
-# key1 = 13
-# key2 = 23
+# key1(BCM) = 13
+# key2(BCM) = 23
 x = input("pin: ")
 button_pin = int(x)
 
 # Setup the button pin as input with pull-up resistor
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+key1 = 1
+
 try:
-    while True:
+     while True:
         # Read the button state
-        button_state = GPIO.input(button_pin)
+        button_pressed = GPIO.input(button_pin) == GPIO.LOW
+        
+        # Check if the button is pressed and the state is not already "button pressed"
+        if button_pressed and not button_state:
+            print("Button pressed")
+            button_state = True
+        # Check if the button is pressed and the state is already "button pressed"
+        elif button_pressed and button_state:
+            print("Button released")
+            button_state = False
+        
+        # Add a small delay to debounce the button (optional)
+        time.sleep(0.1)
 
-        # Check if the button is pressed
-        if button_state == GPIO.LOW:
-            print("Button pressed!")
-        else:
-            print("Button released!")
 
-        # Wait for a short duration to avoid rapid button presses
-        time.sleep(0.2)
+    # while True:
+    #     # Read the button state
+    #     button_state = GPIO.input(button_pin)
+
+    #     # Check for button press
+    #     if button_state and not prev_button_state:
+    #         print("Button pressed")
+    #     elif not button_state and prev_button_state:
+    #         print("Button released")
+
+    #     # Update previous button state
+    #     prev_button_state = button_state
+
+    #     # Add a small delay to debounce the button
+    #     time.sleep(0.1)
+
 
 except KeyboardInterrupt:
     # Clean up GPIO on keyboard interrupt (Ctrl+C)
